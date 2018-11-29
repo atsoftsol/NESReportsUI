b34a0f292977b69be1aaf47b4969837167d0f128
sunil160586 <44019642+sunil160586@users.noreply.github.com>
2018-11-29 21:22:10 +0530

    Update inspection.component.ts

--- src/app/inspection/inspection.component.ts
+++ src/app/inspection/inspection.component.ts
@@ -11,6 +11,20 @@ export class InspectionComponent implements OnInit {
     states: any[] = [];
     districts: any[] = [];
     branches: any[] = [];
+    stateId: number = 0;
+    stateName: string = '';
+    districtId: number = 0;
+    districtName: string = '';
+    branchId: number = 0;
+    branchName: string = '';
+    isAssemblyNotStartedOnTime: boolean = false;
+    isAssemblyNotEndedOnTime: boolean = false;
+    isAssemblyStartedOnTime: boolean = false;
+    isAssemblyEndedOnTime: boolean = false;
+    isAssemblySchoolPrayerConducted: boolean = false;
+    isAssemblyThoughtForTheDayRead: boolean = false;
+    isAssemblyEKIDZSongRead: boolean = false;
+    isAssemblyWarmUpSongRead: boolean = false;
 
     constructor(private calendar: NgbCalendar, private appService: AppService) {
 
@@ -18,7 +32,7 @@ export class InspectionComponent implements OnInit {
 
     ngOnInit() {
         this.date = this.calendar.getToday();
-        this.getStates();
+        //this.getStates();
     };
 
     getStates() {
@@ -31,6 +45,9 @@ export class InspectionComponent implements OnInit {
     };
 
     getDistricts(stateId: string) {
+        this.stateId = parseInt(stateId);
+        this.stateName = this.states.filter((item) => item.stateId === stateId)[0].stateName;
+
         this.appService.getDistricts(stateId)
             .subscribe(data => {
                 this.districts = data;
@@ -40,11 +57,54 @@ export class InspectionComponent implements OnInit {
     };
 
     getBranches(districtId: string) {
+        this.districtId = parseInt(districtId);
+        this.districtName = this.districts.filter((item) => item.districtId === districtId)[0].districtName;
+
         this.appService.getBranches(districtId)
             .subscribe(data => {
                 this.branches = data;
             }, error => {
                 this.branches = [];
             });
+    };
+
+    getSelectedBranch(branchId: string) {
+        this.branchId = parseInt(branchId);
+        this.branchName = this.branches.filter((item) => item.branchId === branchId)[0].branchName;
+    };
+
+    showAssemblyStartTimeDelay() {
+        this.isAssemblyNotStartedOnTime = true;
+    };
+
+    hideAssemblyStartTimeDelay() {
+        this.isAssemblyNotStartedOnTime = false;
+    };
+
+    showAssemblyEndTimeDelay() {
+        this.isAssemblyNotEndedOnTime = true;
+    };
+
+    hideAssemblyEndTimeDelay() {
+        this.isAssemblyNotEndedOnTime = false;
+    };
+
+    saveInspectionData() {
+        let inspectionData: any = {
+            'date': this.date.year + '-' + this.date.month + '-' + this.date.day,
+            'stateId': this.stateId,
+            'state': this.stateName,
+            'districtId': this.districtId,
+            'district': this.districtName,
+            'branchId': this.branchId,
+            'branch': this.branchName,
+            'assembly': {
+                'schoolPrayer': this.isAssemblySchoolPrayerConducted,
+                'ekidzSong': this.isAssemblyEKIDZSongRead,
+                'warmUpSong': this.isAssemblyWarmUpSongRead,
+                'thoughtForTheDay': this.isAssemblyThoughtForTheDayRead
+            }
+        };
+
     };
 }
